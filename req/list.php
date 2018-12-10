1e661fd213eb88776fd3650cac62c0e2abb5cd5a
SugarF0x <malion.torchwood@yandex.ru>
2018-12-10 19:11:49 +0300

    added all the placeholders for $geekbrains and fixed buttons so that they now work properly even without [enabled] parameter (hopefully (yeah, i did not test it (im not apologizing)))

--- req/list.php
+++ req/list.php
@@ -2,7 +2,7 @@
     $minecraft = [
         [
             'name'  => 'ThaumCraft 5',
-            'img'   => 'img/TC5.png',
+            'img'   => 'img/minecraft/TC5.png',
             'desc'  => 'Thaumcraft 5 на версию 1.8.9: магия и пара графических модов, ничего лишнего',
             'date'  => '01.12.2018',
             'dlc'   => 'https://www.dropbox.com/s/aqjpf0dsf253o61/TC5%20Client.rar?dl=0',
@@ -10,7 +10,7 @@
             'dlb'   => 'https://www.dropbox.com/s/qm90pkrz7s4fnnw/TC5%20Backup.rar?dl=0'
         ],[
             'name'  => 'Immsersive Thaumcraft 6',
-            'img'   => 'img/IT6.png',
+            'img'   => 'img/minecraft/IT6.png',
             'desc'  => 'Thaumcraft 6, Astral Sorcery, Immersive Engineering и пара аддонов на него вместе с десятком модификаций как графических, так и утилитарных, делающих процесс приятнее без особых изменений в геймплее',
             'date'  => '30.11.2018',
             'dlc'   => 'https://www.dropbox.com/s/zb1elozkdjbnhak/TI6%20Client.rar?dl=0', 
@@ -18,7 +18,7 @@
             'dlb'   => 'https://www.dropbox.com/s/goiridluk2ksqg7/TI6%20Backup.rar?dl=0'
         ],[
             'name'  => 'TerraFirmaCraft Reborn',
-            'img'   => 'img/TFCR.png',
+            'img'   => 'img/minecraft/TFCR.png',
             'desc'  => 'Террафирма времён своей славы - средневековые исследования и открытия в красивой обёртке из графических модификаций',
             'date'  => '01.12.2018',
             'dlc'   => 'https://www.dropbox.com/s/jp9vipjlhuasoja/TFCR%20Client.rar?dl=0',
@@ -26,7 +26,7 @@
             'dlb'   => 'https://www.dropbox.com/s/9f40g9obpchj0jr/TFCR%20Backup.rar?dl=0'
         ],[
             'name'  => 'TechnoFirmaCraft',
-            'img'   => 'img/TechFC.png',
+            'img'   => 'img/minecraft/TechFC.png',
             'desc'  => 'В Террафирму пришла индустриальная революция: долой мануфактуры - машины в массы. Моды из TFCR остались нетронуты, к ним добавился ряд виузальных аддонов и Immersive Engineering, вместе с которым подъехали соответствующие правки крафтов',
             'date'  => '01.12.2018',
             'dlc'   => 'https://www.dropbox.com/s/drabvqoofvpgk9a/TechFC%20Client.rar?dl=0',
@@ -37,8 +37,23 @@
 
     $geekbrains = [
         [
-            'name'  => '#',
-            'img'   => '#',
+            'name'  => 'Магазин ржомб',
+            'img'   => 'img/geekbrains/rjomba.png',
+            'psd'   => '#',
+            'site'  => '#'
+        ],[
+            'name'  => 'FUrniture',
+            'img'   => 'img/geekbrains/furniture.png',
+            'psd'   => '#',
+            'site'  => '#'
+        ],[
+            'name'  => 'Jazz man',
+            'img'   => 'img/geekbrains/jazz.png',
+            'psd'   => '#',
+            'site'  => '#'
+        ],[
+            'name'  => 'waxom',
+            'img'   => 'img/geekbrains/waxom.png',
             'psd'   => '#',
             'site'  => '#'
         ]
