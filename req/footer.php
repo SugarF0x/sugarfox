6ffdb32ffb8a730a905f870b4cdabdf1ad3a8daf
Sugar_F0x <malion.torchwood@yandex.ru>
2019-03-10 22:08:46 +0300

    Changed $odmenQuotes to $frupQuotes, so as to better represent the list Also added other quotes from frupblvke

--- req/footer.php
+++ req/footer.php
@@ -2,7 +2,7 @@
     <p>
         <?php
             require('req/list.php');
-            echo $odmenQuotes[rand(0,count($odmenQuotes)-1)];
+            echo $frupQuotes[rand(0,count($odmenQuotes)-1)];
         ?>
     </p>
 </footer>
