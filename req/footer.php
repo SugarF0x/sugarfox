10814ce000635798d0294e682a0f23596cdc9bf2
Sugar_F0x <malion.torchwood@yandex.ru>
2019-03-10 22:12:50 +0300

    I never change, do i, huh?
    Fixed and instant bug with footer only displaying the very first quote

--- req/footer.php
+++ req/footer.php
@@ -2,7 +2,7 @@
     <p>
         <?php
             require('req/list.php');
-            echo $frupQuotes[rand(0,count($odmenQuotes)-1)];
+            echo $frupQuotes[rand(0,count($frupQuotes)-1)];
         ?>
     </p>
 </footer>
