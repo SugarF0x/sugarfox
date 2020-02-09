fab45ad6b50eb3daad374ec5f66a9ef1e38efbbb
Sugar_F0x <malion.torchwood@yandex.ru>
2020-02-09 21:39:17 +0300

    Set up Webpack workspace

--- handlers/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_POST['domain']) && $_POST['domain']) {
-    $domain  = $_POST['domain'];
-    $domains = file("../data/frup-users.txt", FILE_IGNORE_NEW_LINES);
-    if (in_array($domain,$domains)) {
-        echo 'SUCCESS';
-    }
-}
