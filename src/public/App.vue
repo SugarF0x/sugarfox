1615ebd8a62f32aeaf328861ecdf6f725730d536
Sugar_F0x <malion.torchwood@yandex.ru>
2020-04-24 21:37:27 +0300

    Main > Enabled Wedding and Snake buttons > Adjusted main.less to accommodate the new layout > Some minor text beautify

--- src/public/App.vue
+++ src/public/App.vue
@@ -1,21 +1,27 @@
 <template>
     <div>
         <Header></Header>
-        <div id="main" class="container pt-5 text-center">
-            <PentaLink disabled img="img/main/community-chest.webp" desc="Общественная казна"></PentaLink>
-            <PentaLink disabled img="img/main/geekbrains.webp" desc="Geekbrains"></PentaLink>
-            <PentaLink disabled img="img/main/minecraft.webp" desc="Minecraft"></PentaLink>
-            <PentaLink disabled img="img/main/snake.webp" desc="Змейка"></PentaLink>
-            <PentaLink disabled img="img/main/wedding.webp" desc="Свадьба"></PentaLink>
-        </div>
+        <main>
+            <section>
+                <h1>Архив</h1>
+                <PentaLink disabled img="img/main/geekbrains.webp" desc="Geekbrains"                          ></PentaLink>
+                <PentaLink          img="img/main/snake.webp"      desc="Змейка"     link="./archive/snake"   ></PentaLink>
+                <PentaLink          img="img/main/wedding.webp"    desc="Свадьба"    link="./archive/wedding" ></PentaLink>
+            </section>
+            <section>
+                <h1>Другое</h1>
+                <PentaLink disabled img="img/main/community-chest.webp" desc="Общественная казна" ></PentaLink>
+                <PentaLink disabled img="img/main/minecraft.webp"       desc="Minecraft"          ></PentaLink>
+            </section>
+        </main>
         <Footer></Footer>
     </div>
 </template>
 
 <script>
     import PentaLink from './components/PentaLink.vue'
-    import Header from './components/Header.vue'
-    import Footer from './components/Footer.vue'
+    import Header    from './components/Header.vue'
+    import Footer    from './components/Footer.vue'
 
     export default {
         data() {
