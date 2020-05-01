9c0ef97f197a0a610967fe19f6acbb5888ddd682
Sugar_F0x <malion.torchwood@yandex.ru>
2020-05-02 02:00:40 +0300

    Added tooltip to Interactive section > On hover it now tells that one has to be logged in so as to access this section's buttons

--- src/public/views/Home.vue
+++ src/public/views/Home.vue
@@ -11,7 +11,9 @@
             <PentaLink img="img/main/minecraft.webp" desc="Minecraft" link="./minecraft" ></PentaLink>
         </section>
         <section>
-            <h1>Интерактив</h1>
+            <h1>
+                Интерактив<span title="Необходим вход" class="noHighlight">*</span>
+            </h1>
             <PentaLink is-disabled="true" img="img/main/community-chest.webp" desc="Общественная казна"                                    ></PentaLink>
             <PentaLink                    img="img/main/chat.webp"            desc="Чат"                link="./chat" auth-required="true" ></PentaLink>
         </section>
