5db9cd3ffa7ce4fbe0a8d11fb9bbd521f7961667
Sugar_F0x <malion.torchwood@yandex.ru>
2020-05-02 02:06:00 +0300

    Community Bank placeholder > Created COmmunityBank view placeholder >> think i need to rework that page now that it can utilise user profiles

---
+++ src/public/views/CommunityBank.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="communityBank">
+        <h1>Страница общественного банка</h1>
+        <h3>по-хорошему, переделать бы её</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CommunityBank"
+    }
+</script>
+
+<style scoped>
+    #communityBank {
+        text-align: center;
+    }
+</style>
