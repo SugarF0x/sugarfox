34444a9005cba94f05cd93f2f01b2961ed9dbc75
Sugar_F0x <malion.torchwood@yandex.ru>
2020-06-10 17:34:08 +0300

    Add sample v-card for root index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
   <v-container>
-
+    <v-layout col>
+      <v-row justify="center">
+        <v-col cols="12" sm="8" md="6">
+          <v-card>
+            <v-card-title>
+              This is the index page
+            </v-card-title>
+            <v-card-text>
+              This card is here only to index the default page position
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-layout>
   </v-container>
 </template>
