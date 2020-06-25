2f569b65c244f23a88578d4b683992495dfef692
Sugar_F0x <malion.torchwood@yandex.ru>
2020-06-25 18:33:47 +0300

    :recycle: move current profile page to profile-testing
    A work on actual profile page begins, tho the current state page may still be useful
    Thus, all the code is moved and a blank page is created for the new profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,58 +1,13 @@
 <template>
-  <v-layout column>
-    <v-row justify="center">
-      <v-col cols="12" sm="8 md=6">
-        <v-card>
-          <v-card-title>
-            <p class="ma-0">
-              This is <span class="secondary--text">Profile</span> page placeholder
-            </p>
-            <v-spacer></v-spacer>
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-icon v-on="on">
-                  {{ methodIcon }}
-                </v-icon>
-              </template>
-              <span>{{ $auth.user.method + ' auth'}}</span>
-            </v-tooltip>
-          </v-card-title>
-          <v-card-text>
-            <p>
-              This page will have info on the <span class="primary--text">logged in user</span>
-              or a prompt to login if not authed
-            </p>
-            <v-list>
-              <v-list-item two-line
-                           v-for="(value, name) in $auth.user"
-                           :key="name"
-              >
-                <v-list-item-content>
-                  <v-list-item-title class="text-capitalize">{{ name }}</v-list-item-title>
-                  <v-list-item-subtitle>{{ value }}</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-layout>
+
   </v-layout>
 </template>
 
 <script>
   export default {
     name: "index.vue",
-    middleware: ['auth'],
-    computed: {
-      methodIcon() {
-        switch (this.$auth.user.method) {
-          case 'local': return 'mdi-account-circle';
-          case 'vk':    return 'mdi-vk';
-          default:      return 'mdi-help';
-        }
-      }
-    }
+    middleware: ['auth']
   }
 </script>
 
