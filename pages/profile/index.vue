da64b64658b8ac7cf49ac58f58b879cfd0e2898c
Sugar_F0x <malion.torchwood@yandex.ru>
2020-06-25 17:46:31 +0300

    :art: Add auth strategy icon to the profile page
    This icon displays the kind of strategy currently used and shows a tooltip on hover

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -7,6 +7,15 @@
             <p class="ma-0">
               This is <span class="secondary--text">Profile</span> page placeholder
             </p>
+            <v-spacer></v-spacer>
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-icon v-on="on">
+                  {{ methodIcon }}
+                </v-icon>
+              </template>
+              <span>{{ $auth.user.method + ' auth'}}</span>
+            </v-tooltip>
           </v-card-title>
           <v-card-text>
             <p>
@@ -34,7 +43,16 @@
 <script>
   export default {
     name: "index.vue",
-    middleware: ['auth']
+    middleware: ['auth'],
+    computed: {
+      methodIcon() {
+        switch (this.$auth.user.method) {
+          case 'local': return 'mdi-account-circle';
+          case 'vk':    return 'mdi-vk';
+          default:      return 'mdi-help';
+        }
+      }
+    }
   }
 </script>
 
